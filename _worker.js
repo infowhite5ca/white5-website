78edb393ae47c015fddf00692183a6c93dc2f84c
infowhite5ca <info.white5.ca@gmail.com>
2026-07-18 12:28:35 -0600

    Use Meta budget sharing compatibility handler

--- _worker.js
+++ _worker.js
@@ -1,12 +1,12 @@
 import coreWorker from "./worker-core.js";
-import { createPausedWindowCampaign } from "./meta-window-campaign.js";
+import { createPausedWindowCampaignWithBudgetFix } from "./meta-window-campaign-budget-fix.js";
 
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
     if (url.pathname === "/api/meta/create-window-campaign") {
-      return createPausedWindowCampaign(request, env);
+      return createPausedWindowCampaignWithBudgetFix(request, env);
     }
 
     return coreWorker.fetch(request, env, ctx);
