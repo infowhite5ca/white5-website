d4cd6911ff93518201dfc210789b1b85f6a9487a
infowhite5ca <info.white5.ca@gmail.com>
2026-07-18 15:34:21 -0600

    Add privacy policy link to public page footers

--- _worker.js
+++ _worker.js
@@ -1,6 +1,12 @@
 import coreWorker from "./worker-core.js";
 import { createPausedWindowCampaignWithBudgetFix } from "./meta-window-campaign-budget-fix.js";
 
+class PrivacyFooterInjector {
+  element(element) {
+    element.append(' | <a href="/privacy.html">Privacy Policy</a>', { html: true });
+  }
+}
+
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
@@ -9,6 +15,15 @@ export default {
       return createPausedWindowCampaignWithBudgetFix(request, env);
     }
 
-    return coreWorker.fetch(request, env, ctx);
+    const response = await coreWorker.fetch(request, env, ctx);
+    const contentType = response.headers.get("content-type") || "";
+
+    if (!contentType.includes("text/html") || url.pathname.startsWith("/meta-admin")) {
+      return response;
+    }
+
+    return new HTMLRewriter()
+      .on("footer .container", new PrivacyFooterInjector())
+      .transform(response);
   },
 };
